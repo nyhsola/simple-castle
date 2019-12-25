a8f4356b1a1f772d1c8b95e30a9eb46ccf766e07
Yaroslav Aloshyn <nyhsola@gmail.com>
2019-12-25 21:12:51 +0200

    Model update, camera position

--- core/src/com/simple/castle/scenes/main/game/add/GameScene.java
+++ core/src/com/simple/castle/scenes/main/game/add/GameScene.java
@@ -1,6 +1,7 @@
 package com.simple.castle.scenes.main.game.add;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.g3d.Environment;
 import com.badlogic.gdx.graphics.g3d.Model;
@@ -9,7 +10,7 @@ import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.loader.G3dModelLoader;
-import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.g3d.model.Node;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.JsonReader;
 import com.simple.castle.scene.Scene;
@@ -21,13 +22,20 @@ import java.util.Map;
 public class GameScene extends Scene {
     private static final String MODELS_PLANE_G_3_DJ = "models/surface.g3dj";
     public static final String CAMERA_POSITION = "CAMERA_POSITION";
+    public static final float CAMERA_SPEED = 0.1f;
 
     private PerspectiveCamera cam;
     private ModelBatch modelBatch;
     private Model model;
     private ModelInstance instance;
-    private CameraInputController camController;
     private Environment environment;
+
+    private final Vector3 tempVector1 = new Vector3();
+
+    private boolean keyUpHolds = false;
+    private boolean keyDownHolds = false;
+    private boolean keyLeftHolds = false;
+    private boolean keyRightHolds = false;
 
     public GameScene(Scene parent) {
         super(parent);
@@ -41,20 +49,66 @@ public class GameScene extends Scene {
         environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
         environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
 
-        cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        cam.position.set(10f, 10f, 10f);
-        cam.lookAt(0, 0, 0);
-        cam.near = 1f;
-        cam.far = 300f;
-        cam.update();
-
         G3dModelLoader modelLoader = new G3dModelLoader(new JsonReader());
         model = modelLoader.loadModel(Gdx.files.internal(MODELS_PLANE_G_3_DJ));
         instance = new ModelInstance(model);
 
-        camController = new CameraInputController(cam);
+        Node node = instance.getNode("RedCube");
+        Vector3 position = node.translation;
 
-        this.setInputProcessor(camController);
+        cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        cam.position.set(position.x,
+                position.y + 7f,
+                position.z + 4f);
+        cam.lookAt(position.x, position.y, position.z);
+
+        cam.near = 1f;
+        cam.far = 300f;
+        cam.update();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        if(Input.Keys.UP == keycode || Input.Keys.W == keycode) {
+            keyUpHolds = true;
+        }
+        if(Input.Keys.DOWN == keycode || Input.Keys.S == keycode) {
+            keyDownHolds = true;
+        }
+        if(Input.Keys.LEFT == keycode || Input.Keys.A == keycode) {
+            keyLeftHolds = true;
+        }
+        if(Input.Keys.RIGHT == keycode || Input.Keys.D == keycode) {
+            keyRightHolds = true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        if(Input.Keys.UP == keycode || Input.Keys.W == keycode) {
+            keyUpHolds = false;
+        }
+        if(Input.Keys.DOWN == keycode || Input.Keys.S == keycode) {
+            keyDownHolds = false;
+        }
+        if(Input.Keys.LEFT == keycode || Input.Keys.A == keycode) {
+            keyLeftHolds = false;
+        }
+        if(Input.Keys.RIGHT == keycode || Input.Keys.D == keycode) {
+            keyRightHolds = false;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        Vector3 scl = tempVector1.set(cam.direction).scl(amount);
+        if(scl.y > 3 || scl.y < 70) {
+            cam.translate(scl);
+            cam.update();
+        }
+        return false;
     }
 
     @Override
@@ -62,6 +116,23 @@ public class GameScene extends Scene {
         Map<String, Object> map = new HashMap<>();
         map.put(CAMERA_POSITION, new Vector3(cam.position));
         toParent(map);
+
+        if(keyUpHolds) {
+            cam.position.z = cam.position.z - CAMERA_SPEED;
+            cam.update();
+        }
+        if(keyDownHolds) {
+            cam.position.z = cam.position.z + CAMERA_SPEED;
+            cam.update();
+        }
+        if(keyRightHolds) {
+            cam.position.x = cam.position.x + CAMERA_SPEED;
+            cam.update();
+        }
+        if(keyLeftHolds) {
+            cam.position.x = cam.position.x - CAMERA_SPEED;
+            cam.update();
+        }
     }
 
     @Override
@@ -74,7 +145,6 @@ public class GameScene extends Scene {
 
     @Override
     public void render() {
-        camController.update();
         modelBatch.begin(cam);
         modelBatch.render(instance, environment);
         modelBatch.end();
