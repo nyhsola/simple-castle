89d887eb210a96c3e48d1d4526211cd5d661bc53
Yaroslav Aloshyn <nyhsola@gmail.com>
2020-03-30 04:37:32 +0300

    Imported blender model [2] Plane

--- core/src/com/simple/castle/launcher/main/bullet/BModelFactory.java
+++ core/src/com/simple/castle/launcher/main/bullet/BModelFactory.java
@@ -10,7 +10,11 @@ import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.physics.bullet.collision.*;
+import com.badlogic.gdx.math.collision.BoundingBox;
+import com.badlogic.gdx.physics.bullet.collision.Collision;
+import com.badlogic.gdx.physics.bullet.collision.btBoxShape;
+import com.badlogic.gdx.physics.bullet.collision.btCollisionObject;
+import com.badlogic.gdx.physics.bullet.collision.btCylinderShape;
 import com.badlogic.gdx.utils.ArrayMap;
 import com.simple.castle.launcher.main.utils.BModelLoader;
 
@@ -71,18 +75,21 @@ public class BModelFactory extends ApplicationAdapter {
                 new Material(ColorAttribute.createDiffuse(Color.MAGENTA))).cylinder(1f, 2f, 1f, 10);
         model = mb.end();
 
+        Model mapModel = BModelLoader.loadModel();
+        Vector3 dimensions = new Vector3();
+        BoundingBox boundingBox = new BoundingBox();
+        mapModel.calculateBoundingBox(boundingBox);
+        boundingBox.getDimensions(dimensions);
+
         ArrayMap<String, BGameObject.Constructor> constructors;
         constructors = new ArrayMap<>(String.class, BGameObject.Constructor.class);
-        constructors.put("ground", new BGameObject.Constructor(model, "ground", new btBoxShape(new Vector3(2.5f, 0.5f, 2.5f)), 0f));
+        constructors.put("ground", new BGameObject.Constructor(mapModel, "Plane", new btBoxShape(dimensions.scl(0.5f)), 0f));
 //        constructors.put("sphere", new BGameObject.Constructor(model, "sphere", new btSphereShape(0.5f), 1f));
 //        constructors.put("box", new BGameObject.Constructor(model, "box", new btBoxShape(new Vector3(0.5f, 0.5f, 0.5f)), 1f));
 //        constructors.put("cone", new BGameObject.Constructor(model, "cone", new btConeShape(0.5f, 2f), 1f));
 //        constructors.put("capsule", new BGameObject.Constructor(model, "capsule", new btCapsuleShape(.5f, 1f), 1f));
         constructors.put("cylinder", new BGameObject.Constructor(model, "cylinder", new btCylinderShape(new Vector3(.5f, 1f, .5f)), 1f));
-
-        Model myModel = BModelLoader.loadModel();
-
-        constructors.put("myground", new BGameObject.Constructor(myModel, "Cube", new btSphereShape(0.5f), 1f));
+//        constructors.put("myground", new BGameObject.Constructor(mapModel, "Cube", new btSphereShape(0.5f), 1f));
 
         return constructors;
     }
