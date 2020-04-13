8ccfe3043a94cb1fe84cd05a40b3d8f0f1ae08d4
Yaroslav Aloshyn <nyhsola@gmail.com>
2020-04-13 03:01:36 +0300

    Move able by space units

--- simple-castle-project/core/src/com/simple/castle/launcher/main/bullet/scene/game/GameScene.java
+++ simple-castle-project/core/src/com/simple/castle/launcher/main/bullet/scene/game/GameScene.java
@@ -1,30 +1,42 @@
 package com.simple.castle.launcher.main.bullet.scene.game;
 
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Quaternion;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ArrayMap;
 import com.simple.castle.launcher.main.bullet.main.GameModels;
 import com.simple.castle.launcher.main.bullet.object.AbstractGameObject;
 import com.simple.castle.launcher.main.bullet.object.GameObjectConstructor;
 import com.simple.castle.launcher.main.bullet.object.unit.KinematicGameObject;
+import com.simple.castle.launcher.main.bullet.object.unit.UnitGameObject;
 import com.simple.castle.launcher.main.bullet.render.GameCamera;
 import com.simple.castle.launcher.main.bullet.render.GameEnvironment;
 import com.simple.castle.launcher.main.bullet.render.GameRenderer;
 import com.simple.castle.launcher.main.bullet.scene.game.physic.GameScenePhysic;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class GameScene extends ScreenAdapter implements InputProcessor {
 
     private final Vector3 tempVector = new Vector3();
+    private final Quaternion tempQuaternion = new Quaternion();
+
+    private final BitmapFont bitmapFont;
+    private final SpriteBatch batch;
+
     private final GameRenderer gameRenderer;
     private final ArrayMap<String, GameObjectConstructor> constructors;
     private final GameScenePhysic gameScenePhysic;
     private final InputMultiplexer inputMultiplexer;
     private final Map<String, AbstractGameObject> sceneGameObjects;
+
+    private final List<UnitGameObject> units = new ArrayList<>();
 
     private GameEnvironment gameEnvironment;
     private GameCamera gameCamera;
@@ -35,13 +47,17 @@ public class GameScene extends ScreenAdapter implements InputProcessor {
         this.gameScenePhysic = new GameScenePhysic();
         this.inputMultiplexer = new InputMultiplexer();
         this.sceneGameObjects = new HashMap<>();
+        this.bitmapFont = new BitmapFont();
+        this.batch = new SpriteBatch();
     }
 
     @Override
     public void render(float delta) {
         gameCamera.update();
+        this.updateUnit();
         gameRenderer.render(gameCamera, sceneGameObjects.values(), gameEnvironment);
         gameScenePhysic.update(gameCamera, Math.min(1f / 30f, delta));
+        this.renderOverlay();
     }
 
     @Override
@@ -74,12 +90,17 @@ public class GameScene extends ScreenAdapter implements InputProcessor {
 
     @Override
     public void hide() {
+        bitmapFont.dispose();
+        batch.dispose();
         gameScenePhysic.dispose();
         sceneGameObjects.forEach((s, gameObject) -> gameObject.dispose());
     }
 
     @Override
     public boolean keyDown(int keycode) {
+        if (Input.Keys.SPACE == keycode) {
+            this.spawnUnit();
+        }
         return inputMultiplexer.keyDown(keycode);
     }
 
@@ -116,5 +137,49 @@ public class GameScene extends ScreenAdapter implements InputProcessor {
     @Override
     public boolean scrolled(int amount) {
         return inputMultiplexer.scrolled(amount);
+    }
+
+    private void spawnUnit() {
+        Vector3 spawner1 = sceneGameObjects.get("Spawner-1").transform.getTranslation(tempVector).cpy();
+        Vector3 castle2 = sceneGameObjects.get("Castle-2").transform.getTranslation(tempVector).cpy();
+
+        UnitGameObject unitGameObject = new UnitGameObject(constructors.get("Unit-1"));
+        unitGameObject.body.setWorldTransform(new Matrix4());
+        unitGameObject.body.translate(spawner1);
+        unitGameObject.body.setLinearVelocity(castle2.sub(spawner1).scl(0.1f));
+
+        gameScenePhysic.addRigidBody(unitGameObject);
+        sceneGameObjects.put("Unit-1" + UUID.randomUUID(), unitGameObject);
+        units.add(unitGameObject);
+    }
+
+    private void updateUnit() {
+
+    }
+
+    private void renderOverlay() {
+        batch.begin();
+        bitmapFont.draw(batch, "Camera position: " +
+                format(gameCamera.position), 0, 20);
+//        if (selected != null) {
+//            bitmapFont.draw(batch, "Selected (model): " +
+//                    "Position: " + format(selected.transform.getTranslation(tmpVector3)) + " " +
+//                    "Rotation: " + format(selected.transform.getRotation(tmpQuaternion)), 0, 40);
+//
+//            bitmapFont.draw(batch, "Selected (physic): " +
+//                    "Position: " + format(selected.body.getWorldTransform().getTranslation(tmpVector3)) + " " +
+//                    "Rotation: " + format(selected.body.getWorldTransform().getRotation(tmpQuaternion)), 0, 60);
+//
+//            bitmapFont.draw(batch, selected.node, 0, 80);
+//        }
+        batch.end();
+    }
+
+    private String format(Vector3 vector3) {
+        return String.format(Locale.getDefault(), "%.2f, %.2f, %.2f", vector3.x, vector3.y, vector3.z);
+    }
+
+    private String format(Quaternion quaternion) {
+        return String.format(Locale.getDefault(), "%.2f, %.2f, %.2f, %.2f", quaternion.x, quaternion.y, quaternion.z, quaternion.w);
     }
 }
