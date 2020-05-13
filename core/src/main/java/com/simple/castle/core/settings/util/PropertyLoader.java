7d7015da966f6dfc1a5bc98568e665dff142584d
Nyhsola <nyhsola@gmail.com>
2020-05-13 22:38:09 +0300

    Removed unnecessary

--- core/src/main/java/com/simple/castle/core/settings/util/PropertyLoader.java
+++ core/src/main/java/com/simple/castle/core/settings/util/PropertyLoader.java
@@ -9,10 +9,6 @@ import java.util.stream.Collectors;
 
 public final class PropertyLoader {
 
-    // TODO: 5/7/2020 JSON from the box of libgdx and remove Guava
-    private static final String SCENES_FOLDER = "scenes/";
-    private static final String APP_PROPERTIES = "app.properties";
-
     private PropertyLoader() {
     }
 
@@ -36,99 +32,4 @@ public final class PropertyLoader {
         return properties;
     }
 
-//    public static Properties loadProperties(String fromScene) {
-//        return getProperties(SCENES_FOLDER + findScene(fromScene) + "/scene.properties");
-//    }
-//
-//    public static List<Map<String, Object>> loadConstructors(String fromScene) {
-//        return loadListMap(SCENES_FOLDER + findScene(fromScene) + "/object-constructors.json",
-//                "models");
-//    }
-//
-//    public static List<com.simple.castle.core.settings.dto.base.SceneObjectJson> loadObjects(String fromScene) {
-//        return loadListMap(SCENES_FOLDER + findScene(fromScene) + "/scene-objects.json", "scene-objects")
-//                .stream()
-//                .map(listMap -> {
-//                    com.simple.castle.core.settings.dto.base.SceneObjectJson sceneObjectJson = new SceneObjectJson();
-//                    sceneObjectJson.setModel((String) listMap.get("model"));
-//                    sceneObjectJson.setInteract((String) listMap.get("interact"));
-//                    sceneObjectJson.setHide(String.valueOf(listMap.get("hide")));
-//                    return sceneObjectJson;
-//                })
-//                .collect(Collectors.toList());
-//    }
-//
-//    public static List<com.simple.castle.core.settings.dto.base.PlayerJson> loadPlayers(String fromScene) {
-//        return loadListMap(SCENES_FOLDER + findScene(fromScene) + "/players.json", "players")
-//                .stream()
-//                .map(listMap -> {
-//                    com.simple.castle.core.settings.dto.base.PlayerJson playerJson = new PlayerJson();
-//                    playerJson.setPlayerName((String) listMap.get("player-name"));
-//                    playerJson.setUnitType((String) listMap.get("unit-type"));
-//                    List<List<String>> paths = castToList(listMap.get("paths"))
-//                            .stream()
-//                            .map(PropertyLoader::castToMap)
-//                            .map(map -> map.get("path"))
-//                            .map(path -> (String) path)
-//                            .map(pathString -> Arrays.asList(pathString.split(" ")))
-//                            .collect(Collectors.toList());
-//                    playerJson.setPaths(paths);
-//                    return playerJson;
-//                })
-//                .collect(Collectors.toList());
-//    }
-//
-//    private static String findScene(String sceneName) {
-//        String scenesFile = (String) getProperties(APP_PROPERTIES).get("scenes-file");
-//        return (String) new JSONObject(loadResource(scenesFile))
-//                .getJSONArray("scenes")
-//                .toList()
-//                .stream()
-//                .filter(scene -> scene.equals(sceneName))
-//                .collect(Collectors.toSet())
-//                .stream()
-//                .findFirst()
-//                .orElseThrow(() -> new AssertionError("Missing scene properties"));
-//    }
-//
-//    private static Properties getProperties(String resource) {
-//        Properties properties = new Properties();
-//        try {
-//            properties.load(loadInputStream(resource));
-//            return properties;
-//        } catch (IOException exception) {
-//            throw new AssertionError("Missing file", exception);
-//        }
-//    }
-//
-//    private static List<Map<String, Object>> loadListMap(String jsonFile, String array) {
-//        return new JSONObject(loadResource(jsonFile))
-//                .getJSONArray(array)
-//                .toList()
-//                .stream()
-//                .map(PropertyLoader::castToMap)
-//                .collect(Collectors.toList());
-//    }
-//
-//    private static String loadResource(String resource) {
-//        try {
-//            return CharStreams.toString(new InputStreamReader(loadInputStream(resource)));
-//        } catch (IOException exception) {
-//            throw new AssertionError("Missing file", exception);
-//        }
-//    }
-//
-//    private static InputStream loadInputStream(String resource) {
-//        return PropertyLoader.class.getResourceAsStream(resource);
-//    }
-//
-//    @SuppressWarnings("unchecked")
-//    private static Map<String, Object> castToMap(Object object) {
-//        return ((Map<String, Object>) object);
-//    }
-//
-//    @SuppressWarnings("unchecked")
-//    private static List<Object> castToList(Object object) {
-//        return ((List<Object>) object);
-//    }
 }
--- core/src/main/java/com/simple/castle/scene/game/GameSettings.java
+++ core/src/main/java/com/simple/castle/scene/game/GameSettings.java
@@ -1,7 +1,6 @@
 package com.simple.castle.scene.game;
 
 import com.badlogic.gdx.utils.Json;
-import com.simple.castle.core.settings.AppSettings;
 import com.simple.castle.core.settings.dto.ObjectConstructorsJson;
 import com.simple.castle.core.settings.dto.PlayersJson;
 import com.simple.castle.core.settings.dto.SceneObjectsJson;
@@ -10,17 +9,15 @@ import com.simple.castle.core.settings.util.PropertyLoader;
 import java.util.Properties;
 
 public class GameSettings {
-    public static final PlayersJson PLAYERS_JSON = new Json().fromJson(PlayersJson.class, PropertyLoader.loadData("/scenes/game/json/players.json"));
-    public static final ObjectConstructorsJson OBJECT_CONSTRUCTORS_JSON = new Json().fromJson(ObjectConstructorsJson.class, PropertyLoader.loadData("/scenes/game/json/object-constructors.json"));
-    public static final SceneObjectsJson SCENE_OBJECTS_JSON = new Json().fromJson(SceneObjectsJson.class, PropertyLoader.loadData("/scenes/game/json/scene-objects.json"));
     private static final Properties GAME = PropertyLoader.load("/scenes/game/scene.properties");
+
     public static final String CAMERA_BASE_PLANE = GAME.getProperty("camera-base-plane");
     public static final String CAMERA_INIT_POSITION_FROM = GAME.getProperty("camera-init-position-from");
-
-    static {
-        if (!AppSettings.SCENES_JSON.getAvailableScenes().contains("game")) {
-            throw new RuntimeException("Missing game settings!");
-        }
-    }
+    public static final PlayersJson PLAYERS_JSON = new Json()
+            .fromJson(PlayersJson.class, PropertyLoader.loadData("/scenes/game/json/players.json"));
+    public static final ObjectConstructorsJson OBJECT_CONSTRUCTORS_JSON = new Json()
+            .fromJson(ObjectConstructorsJson.class, PropertyLoader.loadData("/scenes/game/json/object-constructors.json"));
+    public static final SceneObjectsJson SCENE_OBJECTS_JSON = new Json()
+            .fromJson(SceneObjectsJson.class, PropertyLoader.loadData("/scenes/game/json/scene-objects.json"));
 
 }
