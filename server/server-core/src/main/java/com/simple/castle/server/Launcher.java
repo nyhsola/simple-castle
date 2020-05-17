cf2b2e5e083d1261b30aa838ed401bdf888e4fd7
Nyhsola <nyhsola@gmail.com>
2020-05-17 04:15:18 +0300

    Server socket

--- server/server-core/src/main/java/com/simple/castle/server/Launcher.java
+++ server/server-core/src/main/java/com/simple/castle/server/Launcher.java
@@ -1,9 +1,24 @@
 package com.simple.castle.server;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Net;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.net.ServerSocket;
+import com.badlogic.gdx.net.ServerSocketHints;
+import com.badlogic.gdx.net.Socket;
 
 public class Launcher {
     public static void main(String[] args) {
         new HeadlessApplication(new CastleGame());
+        Gdx.app.log("Server", "Started!");
+
+        new Thread(() -> {
+            ServerSocketHints serverSocketHints = new ServerSocketHints();
+            serverSocketHints.acceptTimeout = 0;
+            ServerSocket serverSocket = Gdx.net.newServerSocket(Net.Protocol.TCP, 9090, serverSocketHints);
+            while (!Thread.currentThread().isInterrupted()) {
+                Socket socket = serverSocket.accept(null);
+            }
+        }).start();
     }
 }
